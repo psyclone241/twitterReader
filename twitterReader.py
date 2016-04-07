3ea0602cc729dd75d9e2f30ebf2b9d0baa6f5bda
Roy Keyes <keyes@ufl.edu>
2016-04-07 11:15:39 -0400

    Refine parameters and output_types

--- twitterReader.py
+++ twitterReader.py
@@ -26,6 +26,7 @@ class twitterReader:
             )
 
         parser.add_argument('-f', action='store', dest='specific_feed', help='Read just one feed', default=None)
+        parser.add_argument('-s', action='store', dest='save_file', help='Save the output to file named....', default=None)
         parser.add_argument('-o', action='store', dest='output_type', help='What type of output do you prefer?', default=config.app['default_output'])
         parser.add_argument('-c', type=int, action='store', dest='count', help='Read n items from specified feed', choices=config.app['count_ranges'])
         args = parser.parse_args()
@@ -37,6 +38,11 @@ class twitterReader:
             self.specific_count = args.count
         else:
             self.specific_count = config.app['default_count']
+
+        if args.save_file:
+            self.save_file = args.save_file
+        else:
+            self.save_file = None
 
         if args.output_type:
             self.output_type = args.output_type
@@ -56,15 +62,37 @@ class twitterReader:
 
             if not self.specific_feed:
                 if config.feeds:
-                    for feed in config.feeds:
-                        if feed in config.feed_config:
-                            self.getFeed(twitter, feed, config.feed_config[feed]['count'])
+                    if self.output_type == 'print':
+                        for feed in config.feeds:
+                            if feed in config.feed_config:
+                                print(self.getFeed(twitter, feed, config.feed_config[feed]['count']))
+                            else:
+                                print(self.getFeed(twitter, feed, self.specific_count))
+                    else:
+                        data = {}
+                        for feed in config.feeds:
+                            if feed in config.feed_config:
+                                data[feed] = self.getFeed(twitter, feed, config.feed_config[feed]['count'])
+                            else:
+                                data[feed] = self.getFeed(twitter, feed, self.specific_count)
+
+                        if self.output_type == 'object':
+                            return data
                         else:
-                            self.getFeed(twitter, feed, self.specific_count)
+                            json_data = json.dumps(data, indent=4)
+                            if self.save_file:
+                                with open(self.save_file, 'w') as save_file:
+                                    save_file.write(json_data)
+                            else:
+                                return json_data
                 else:
                     print('No feeds specified')
             else:
-                self.getFeed(twitter, self.specific_feed, self.specific_count)
+                data = self.getFeed(twitter, self.specific_feed, self.specific_count)
+                if self.output_type == 'object':
+                    return data
+                else:
+                    print(data)
 
         except TwitterHTTPError as twitterError:
             print('There was an authentication error, re-enter your credentials')
@@ -80,15 +108,15 @@ class twitterReader:
         entries = twitterObject.statuses.user_timeline(screen_name=screen_name, count=count)
         lookup = twitterObject.users.lookup(screen_name=screen_name)
 
-        if self.output_type == 'object':
+        if self.output_type == 'object' or self.output_type == 'json':
             return entries
-        elif self.output_type == 'json':
-            print(json.dumps(entries, indent=4))
         else:
+            data = ''
             if self.print_headers:
-                print('-------------Twitter Feed for @' + screen_name + ' (' + lookup[0]['name'] + ') ------------- ')
+                data += '-------------Twitter Feed for @' + screen_name + ' (' + lookup[0]['name'] + ') -------------' + "\n"
             for entry in entries:
-                print(entry['created_at'] + ' -> ' + entry['text'])
+                data += entry['created_at'] + ' -> ' + entry['text'] + "\n"
+            return data
 
     def dumpclean(self, obj):
         if type(obj) == dict:
